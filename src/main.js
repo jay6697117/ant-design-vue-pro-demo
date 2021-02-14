9172d6f61bd05513a3e2c192b6a141d2bec2cc82
steve zhang <871506263@qq.com>
2021-02-14 22:02:23 +0800

    提交代码于: 2021-02-14 22:02:23

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Antdv from 'ant-design-vue';
-console.log('Antdv :>> ', Antdv);
+import Antd from 'ant-design-vue';
+console.log('Antd :>> ', Antd);
 import 'ant-design-vue/dist/antd.less';
-Vue.use(Antdv);
+Vue.use(Antd);
 
 Vue.config.productionTip = false;
 
