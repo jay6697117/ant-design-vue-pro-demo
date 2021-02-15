8a903c75bc34935131d61785e6251a6129e1ac6d
steve zhang <871506263@qq.com>
2021-02-15 20:45:44 +0800

    提交代码于: 2021-02-15 20:45:44

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const terserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -5,5 +7,24 @@ module.exports = {
         javascriptEnabled: true
       }
     }
+  },
+  devServer: {
+    open: true
+  },
+  configureWebpack: config => {
+    config.optimization = {
+      minimizer: [
+        new terserPlugin({
+          terserOptions: {
+            compress: {
+              warnings: false,
+              drop_console: true, //去除console
+              drop_debugger: true, //去除debugger
+              pure_funcs: ['console.log']
+            }
+          }
+        })
+      ]
+    };
   }
 };
