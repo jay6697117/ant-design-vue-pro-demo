724478a43f29ac9083dcd44bd6e13a412867c15c
steve zhang <871506263@qq.com>
2021-02-15 22:37:38 +0800

    提交代码于: 2021-02-15 22:37:38

--- vue.config.js
+++ vue.config.js
@@ -28,16 +28,6 @@ module.exports = {
   devServer: {
     open: true,
     host: '0.0.0.0',
-    port: 8080,
-    proxy: {
-      '/': {
-        target: 'http://127.0.0.1:8080/',
-        changeOrigin: true, //是否开启跨域
-        ws: false, //是否支持WebSocket
-        pathRewrite: {
-          '^/': ''
-        }
-      }
-    }
+    port: 8080
   }
 };
