6fa7cafcc9e570d5a3f6254f7953df4771e49ced
steve zhang <871506263@qq.com>
2021-02-16 12:31:33 +0800

    提交代码于: 2021-02-16 12:31:32

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.less';
-Vue.use(Antd);
+// import Antd from 'ant-design-vue';
+// console.log('Antd :>> ', Antd);
+import { Button, Modal } from 'ant-design-vue';
+console.log('Button :>> ', Button);
+console.log('Modal :>> ', Modal);
+// import 'ant-design-vue/dist/antd.less';
+import 'ant-design-vue/lib/button/style/index.less';
+import 'ant-design-vue/lib/modal/style/index.less';
+
+// Vue.use(Antd);
+Vue.use(Button);
+Vue.use(Modal);
 
 Vue.config.productionTip = false;
 
